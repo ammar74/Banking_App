29e89fc6bb56c9ad08f17707504743832ce0269c
Ammar74 <ammaryasser3016@gmail.com>
2023-06-03 17:00:20 +0300

    * adding about fragment * edit navigation Logic

--- app/src/main/java/com/example/bankingapp/MainActivity.kt
+++ app/src/main/java/com/example/bankingapp/MainActivity.kt
@@ -3,43 +3,21 @@ package com.example.bankingapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
-import com.example.bankingapp.databinding.ActivityMainBinding
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //Splash Screen
         installSplashScreen()
+        setContentView(R.layout.activity_main)
 
-        //DataBinding
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val bottomNavigation :BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val navController=findNavController(R.id.fragmentContainerView)
+        bottomNavigation.setupWithNavController(navController)
 
-        //Default Fragment
-        loadFragment(CustomersFragment())
-
-        //Navigation Logic
-        binding.bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.customers_fragment -> loadFragment(CustomersFragment())
-                R.id.transactions_fragment -> loadFragment(TransactionsFragment())
-                R.id.about_fragment -> loadFragment(AboutFragment())
-                else ->{
-                    false
-                }
-            }
-            true
-        }
     }
 
-    private fun loadFragment(fragment: Fragment) {
-        val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.frame_container, fragment)
-        transaction.addToBackStack(null)
-        transaction.commit()
-    }
 }
